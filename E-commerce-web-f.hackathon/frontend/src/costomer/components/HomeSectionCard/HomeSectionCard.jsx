33ea87ec2c648468e1d29589553442df564f7150
ANUMARIF <155381975+anumarif833@users.noreply.github.com>
2025-04-23 04:08:56 +0500

    Add files via upload
    dun

---
+++ E-commerce-web-f.hackathon/frontend/src/costomer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const HomeSectionCard = () => {
+  return (
+    
+    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
+      
+    <div className='h-[13rem] w-[10rem]'>
+    <img className='object-cover object-top w-full h-full' src="https://images-na.ssl-images-amazon.com/images/I/71qS1txuu0L._AC_SR185,210,0,B_BR3_.jpg" alt="" />
+    
+    </div>
+
+    <div className='p-4'>
+        <h3>Nofilter</h3>
+       
+    </div>
+        </div>
+    
+  )
+}
+
+export default HomeSectionCard
+
---
+++ E-commerce-web-f.hackathon/frontend/src/costomer/pages/HomePages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarosel from '../../HomeCarosel/MainCarosel'
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarosel/>
+      <div>
+        Other Callaction
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
